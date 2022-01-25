0d959d31db8a0760702d53782262691f430dc232
August Sosick <asosick@Augusts-MacBook-Pro.local>
2022-01-24 23:09:08 -0700

    master  first commit with new util

--- sick.c
+++ sick.c
@@ -1,11 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int system(const char *command);
+const char* getCurrentBranch();
+void autoCommit(const char* branch, const char* commitMsg);
 
 int main(int argc, char **argv){
-    char** s = argv + 1;
-    while(*s != NULL){
-        printf("Args: %s\n", *s);
-        ++s;
-    }
+    char** s = argv + sizeof(char);
+    char* commitMsg = *(++argv);
+    printf("Your commit message is %s\n", commitMsg);
+    getCurrentBranch();
+    const char* branch = getCurrentBranch();
+    printf("Your current branch is %s\n", branch);
 
+    autoCommit(branch, commitMsg);
+
+    free(branch);
     return 0;
+}
+
+void autoCommit(const char* branch, const char* commitMsg){
+    char* buffer[strlen(branch) + strlen(commitMsg) + 25];
+    sprintf(buffer, "git commit -m \'%s %s\'", branch, commitMsg);
+    printf("running: %s\n", buffer);
+    system(buffer);
+}
+
+const char* getCurrentBranch(){
+    FILE* fp;
+    char buffer[128];
+    fp = popen("git branch --show-current", "r");
+    if(fp == NULL){
+        printf("Failed to fetch git branch");
+        fclose(fp);
+        exit(1);
+    }
+    
+    char* ans;
+    if(fgets(buffer, sizeof(buffer), fp) != NULL){
+        ans = strdup( buffer);
+    }
+    fclose(fp);
+    return ans;
 }
